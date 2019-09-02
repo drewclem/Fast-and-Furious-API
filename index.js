69432eb8499ce8b93e08e83d328b356298c40b2f
Drew Clements <drewclementsdesign@gmail.com>
2019-09-02 14:33:11 -0400

    work work work

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const fs = require('fs')
 
+app.set('view engine', 'ejs')
+
 app.get("/cars", (req, res) => {
   let carData = JSON.parse(fs.readFileSync('cars.json', 'utf-8'))
 

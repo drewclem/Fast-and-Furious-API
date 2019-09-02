e09732fa32f3dfbf2f3430dded16cbcec50cd619
Drew Clements <drewclementsdesign@gmail.com>
2019-09-02 14:09:49 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const fs = require('fs')
+
+app.get("/cars", (req, res) => {
+  let carData = JSON.parse(fs.readFileSync('cars.json', 'utf-8'))
+
+  res.json(carData)
+})
+
+app.get('/drivers', (req, res) => {
+  let driverData = JSON.parse(fs.readFileSync('drivers.json', 'utf-8'))
+
+  res.json(driverData)
+})
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+})

b69ea26c996e909a7e080537d6fe36862f74f1e5
Drew Clements <drewclementsdesign@gmail.com>
2019-09-02 14:47:39 -0400

    IT IS ALIVE!

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const fs = require('fs')
 
+app.get("/", (req, res) => {
+  res.send('Welcome to my Fast and Furious API! A work in progress indeed. Visit "/cars" for car data and "/drivers" for character data.')
+})
+
 app.get("/cars", (req, res) => {
   let carData = JSON.parse(fs.readFileSync('cars.json', 'utf-8'))
 
